6fb6b3a9c34d601a36155f2e9275118dd9855484
SilasDemez <silasdemez@gmail.com>
2021-05-10 11:16:46 +0200

    start chatprogramm

---
+++ multiclient_chat/client.py
@@ -0,0 +1,179 @@
+# coding=utf-8
+import socket, threading
+import tkinter as tk
+
+# nickname = input("Wähle deinen Nicknamen: ")
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
+# client.connect(('127.0.0.1', 7976))  # connecting client to server
+
+FORMAT = "utf-8"
+
+
+# GUI class for the chat
+class GUI:
+    # constructor method
+    def __init__(self):
+
+        # chat window which is currently hidden
+        self.window = tk.Tk
+
+        l1 = tk.Label(text="Hello")
+        l1.pack()
+
+        # self.Window.withdraw()
+
+        self.window.mainloop()
+"""
+    def nextWindow(self, name):
+        self.login.destroy()
+        self.layout(name)
+
+        # the thread to receive messages
+        rcv = threading.Thread(target=self.receive)
+        rcv.start()
+
+    # The main layout of the chat
+    def layout(self, name):
+
+        self.name = name
+        # to show chat window
+        self.Window.deiconify()
+        self.Window.title("CHATROOM")
+        self.Window.resizable(width=False,
+                              height=False)
+        self.Window.configure(width=470,
+                              height=550,
+                              bg="#17202A")
+        self.labelHead = Label(self.Window,
+                               bg="#17202A",
+                               fg="#EAECEE",
+                               text=self.name,
+                               font="Helvetica 13 bold",
+                               pady=5)
+
+        self.labelHead.place(relwidth=1)
+        self.line = Label(self.Window,
+                          width=450,
+                          bg="#ABB2B9")
+
+        self.line.place(relwidth=1,
+                        rely=0.07,
+                        relheight=0.012)
+
+        self.textCons = Text(self.Window,
+                             width=20,
+                             height=2,
+                             bg="#17202A",
+                             fg="#EAECEE",
+                             font="Helvetica 14",
+                             padx=5,
+                             pady=5)
+
+        self.textCons.place(relheight=0.745,
+                            relwidth=1,
+                            rely=0.08)
+
+        self.labelBottom = Label(self.Window,
+                                 bg="#ABB2B9",
+                                 height=80)
+
+        self.labelBottom.place(relwidth=1,
+                               rely=0.825)
+
+        self.entryMsg = Entry(self.labelBottom,
+                              bg="#2C3E50",
+                              fg="#EAECEE",
+                              font="Helvetica 13")
+
+        # place the given widget
+        # into the gui window
+        self.entryMsg.place(relwidth=0.74,
+                            relheight=0.06,
+                            rely=0.008,
+                            relx=0.011)
+
+        self.entryMsg.focus()
+
+        # create a Send Button
+        self.buttonMsg = Button(self.labelBottom,
+                                text="Send",
+                                font="Helvetica 10 bold",
+                                width=20,
+                                bg="#ABB2B9",
+                                command=lambda: self.sendButton(self.entryMsg.get()))
+
+        self.buttonMsg.place(relx=0.77,
+                             rely=0.008,
+                             relheight=0.06,
+                             relwidth=0.22)
+
+        self.textCons.config(cursor="arrow")
+
+        # create a scroll bar
+        scrollbar = Scrollbar(self.textCons)
+
+        # place the scroll bar
+        # into the gui window
+        scrollbar.place(relheight=1,
+                        relx=0.974)
+
+        scrollbar.config(command=self.textCons.yview)
+
+        self.textCons.config(state=DISABLED)
+
+    # function to basically start the thread for sending messages
+    def sendButton(self, msg):
+        self.textCons.config(state=DISABLED)
+        self.msg = msg
+        self.entryMsg.delete(0, END)
+        snd = threading.Thread(target=self.sendMessage)
+        snd.start()
+
+        def receive():
+            while True:  # making valid connection
+                try:
+                    message = client.recv(1024).decode('utf8')
+                    if message == 'NICKNAME':
+                        client.send(nickname.encode('utf8'))
+                    else:
+                        print(message)
+                except:  # case on wrong ip/port details
+                    print("FATAL ERROR!")
+                    client.close()
+                    break
+
+    # function to receive messages
+    def receive(self):
+        while True:
+            try:
+                message = client.recv(1024).decode(FORMAT)
+
+                # if the messages from the server is NAME send the client's name
+                if message == 'NICKNAME':
+                    client.send(self.name.encode(FORMAT))
+                else:
+                    # insert messages to text box
+                    self.textCons.config(state=NORMAL)
+                    self.textCons.insert(END,
+                                         message + "\n\n")
+
+                    self.textCons.config(state=DISABLED)
+                    self.textCons.see(END)
+            except:
+                # an error will be printed on the command line or console if there's an error
+                print("An error occured!")
+                client.close()
+                break
+
+                # function to send messages
+
+    def sendMessage(self):
+        self.textCons.config(state=DISABLED)
+        while True:
+            message = (f"{self.name}: {self.msg}")
+            client.send(message.encode(FORMAT))
+            break
+
+"""
+# create a GUI class object
+g = GUI()
---
+++ multiclient_chat/server.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+import socket, threading
+
+host = '127.0.0.1'  # LocalHost
+port = 7976  # Port wählen
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialisieren
+server.bind((host, port))  # binding host und port zu einem socket
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):  # broadcast funktion
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:  # recieving messages vom Client
+            message = client.recv(1024)
+            broadcast(message)
+        except:  # löschen der Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} hat verlassen!'.format(nickname).encode('utf8'))
+            nicknames.remove(nickname)
+            break
+
+
+def receive():  # mehrere Clients empfangen
+    while True:
+        client, address = server.accept()
+        print("Verbunden mit {}".format(str(address)))
+        client.send('NICKNAME'.encode('utf8'))
+        nickname = client.recv(1024).decode('utf8')
+        nicknames.append(nickname)
+        clients.append(client)
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('utf8'))
+        client.send('Connected to server!'.encode('utf8'))
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+receive()
